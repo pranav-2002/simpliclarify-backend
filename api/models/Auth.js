480dec373d5c7f51d757384d3dea41e05cdbdfb6
Pranav <pranav.2002tis@gmail.com>
2024-05-07 01:06:11 +0530

    Fixed AWS SES Issue

--- api/models/Auth.js
+++ api/models/Auth.js
@@ -1,42 +1,42 @@
-'use strict'
+'use strict';
 
-import crypto from 'crypto'
-import generator from 'generate-password'
+import crypto from 'crypto';
+import generator from 'generate-password';
 
-import { CustomError } from '../helpers'
+import { CustomError } from '../helpers';
 
-import User from '../schemas/User'
-import Referral from '../schemas/Referral'
-import Transaction from '../schemas/Transaction'
-import { sendMail } from '../../email-templates/sendEmailTemplate'
-import Methods from '../helpers/Methods'
-import Constants from '../../config/CONSTANTS'
-import signupEmailTemplate from '../../email-templates/signupEmailTemplate'
-import verifyUserEmailTemplate from '../../email-templates/verifyUserEmailTemplate'
-import forgotPasswordEmailTemplate from '../../email-templates/forgotPasswordEmailTemplate'
+import User from '../schemas/User';
+import Referral from '../schemas/Referral';
+import Transaction from '../schemas/Transaction';
+import { sendMail } from '../../email-templates/sendEmailTemplate';
+import Methods from '../helpers/Methods';
+import Constants from '../../config/CONSTANTS';
+import signupEmailTemplate from '../../email-templates/signupEmailTemplate';
+import verifyUserEmailTemplate from '../../email-templates/verifyUserEmailTemplate';
+import forgotPasswordEmailTemplate from '../../email-templates/forgotPasswordEmailTemplate';
 
-const AuthModel = { login, signUp, verifyUser, forgotPassword, googleLogin }
+const AuthModel = { login, signUp, verifyUser, forgotPassword, googleLogin };
 
-export default AuthModel
+export default AuthModel;
 
 async function dbCallHandler(dbCall, cError) {
     try {
-        return await dbCall
+        return await dbCall;
     } catch (error) {
-        throw new CustomError(cError)
+        throw new CustomError(cError);
     }
 }
 
 async function googleLogin(body) {
     try {
-        const { userName, userEmail, googleToken } = body
+        const { userName, userEmail, googleToken } = body;
         if (!userEmail || userEmail == '') {
-            throw new CustomError('Enter valid email id')
+            throw new CustomError('Enter valid email id');
         }
-        let user = await User.findOne({ userEmail })
+        let user = await User.findOne({ userEmail });
         if (!user) {
             // CREATE A NEW USER
-            let userReferralCode = await _generateReferralCode()
+            let userReferralCode = await _generateReferralCode();
             let newUser = new User({
                 userName,
                 userEmail,
@@ -44,63 +44,68 @@ async function googleLogin(body) {
                 wallet: Constants.joiningAmountWithoutReferral,
                 userVerify: true,
                 isOAuth: true,
-                userReferralCode
-            })
-            await newUser.save()
-            await addTransaction(newUser, Constants.joiningAmountWithoutReferral, 'Signup Reward')
-            newUser = JSON.parse(JSON.stringify(newUser))
-            delete newUser['googleToken']
-            return newUser
+                userReferralCode,
+            });
+            await newUser.save();
+            await addTransaction(
+                newUser,
+                Constants.joiningAmountWithoutReferral,
+                'Signup Reward'
+            );
+            newUser = JSON.parse(JSON.stringify(newUser));
+            delete newUser['googleToken'];
+            return newUser;
         } else {
             if (!user.isOAuth) {
                 throw new CustomError({
                     statusCode: 403,
-                    message: 'Account with same email id already exists! Try signing in directly / use forgot password',
-                })
+                    message:
+                        'Account with same email id already exists! Try signing in directly / use forgot password',
+                });
             }
 
             if (user.googleToken !== googleToken) {
                 throw new CustomError({
                     statusCode: 403,
                     message: 'Invalid google token provided',
-                })
+                });
             }
 
-            user = JSON.parse(JSON.stringify(user))
-            delete user['googleToken']
-            return user
+            user = JSON.parse(JSON.stringify(user));
+            delete user['googleToken'];
+            return user;
         }
     } catch (err) {
-        throw new CustomError(err)
+        throw new CustomError(err);
     }
 }
 
 async function signUp(body) {
-    const { userEmail, userContactNumber, referralCode } = body
+    const { userEmail, userContactNumber, referralCode } = body;
     const foundUserByMailId = await dbCallHandler(
         User.findOne({ userEmail }),
         'User not found'
-    )
+    );
 
     if (foundUserByMailId && foundUserByMailId.userEmail === userEmail) {
         if (foundUserByMailId.userContactNumber === userContactNumber) {
             throw new CustomError({
                 statusCode: 403,
                 message: 'Account already registered with this contact number',
-                code: 'SIMPLICLARIFY_AUTH_04'
-            })
+                code: 'SIMPLICLARIFY_AUTH_04',
+            });
         } else {
             throw new CustomError({
                 statusCode: 403,
                 message: 'Account already registered with this email address',
-                code: 'SIMPLICLARIFY_AUTH_04'
-            })
+                code: 'SIMPLICLARIFY_AUTH_04',
+            });
         }
     }
     const foundUserByContact = await dbCallHandler(
         User.findOne({ userContactNumber }),
         'User not found'
-    )
+    );
 
     if (
         foundUserByContact &&
@@ -109,188 +114,220 @@ async function signUp(body) {
         throw new CustomError({
             statusCode: 403,
             message: 'Account already registered with this contact number',
-            code: 'SIMPLICLARIFY_AUTH_04'
-        })
+            code: 'SIMPLICLARIFY_AUTH_04',
+        });
     }
-    const getHash = _generateHash(userEmail)
+    const getHash = _generateHash(userEmail);
 
     try {
-        let getReferralCode = await _generateReferralCode()
+        let getReferralCode = await _generateReferralCode();
 
-        Object.assign(body, { verifyHash: getHash })
-        Object.assign(body, { userReferralCode: getReferralCode })
-        const newUser = new User(body)
+        Object.assign(body, { verifyHash: getHash });
+        Object.assign(body, { userReferralCode: getReferralCode });
+        const newUser = new User(body);
 
         async function verifyReferralCode(referralCode, newUser) {
             if (!referralCode) {
-                return
+                return;
             }
             const userReferrer = await User.findOne({
-                userReferralCode: referralCode
-            })
+                userReferralCode: referralCode,
+            });
             if (userReferrer) {
-                newUser.referralCodeUsed = referralCode
-                return
+                newUser.referralCodeUsed = referralCode;
+                return;
             }
 
             const adminReferrer = await Referral.findOne({
-                code: referralCode
-            })
+                code: referralCode,
+            });
             if (adminReferrer) {
-                if (adminReferrer.currentCount >= adminReferrer.limit || !adminReferrer.active) {
+                if (
+                    adminReferrer.currentCount >= adminReferrer.limit ||
+                    !adminReferrer.active
+                ) {
                     throw new CustomError({
                         statusCode: 401,
-                        message: 'The Referral Code provided is no longer valid.',
-                        code: 'SIMPLICLARIFY_AUTH_05'
-                    })
+                        message:
+                            'The Referral Code provided is no longer valid.',
+                        code: 'SIMPLICLARIFY_AUTH_05',
+                    });
                 }
-                adminReferrer.currentCount += 1
-                await adminReferrer.save()
-                newUser.referralCodeUsed = referralCode
-                return
+                adminReferrer.currentCount += 1;
+                await adminReferrer.save();
+                newUser.referralCodeUsed = referralCode;
+                return;
             }
 
             throw new CustomError({
                 statusCode: 401,
                 message: 'Invalid Referral Code',
-                code: 'SIMPLICLARIFY_AUTH_05'
-            })
+                code: 'SIMPLICLARIFY_AUTH_05',
+            });
         }
-        await verifyReferralCode(referralCode, newUser)
-        const endPoint = `https://simpliclarify.com/user-verification/${getHash}`
-        const mailBody = signupEmailTemplate(endPoint, newUser.userName)
+        await verifyReferralCode(referralCode, newUser);
+        const endPoint = `https://simpliclarify.com/user-verification/${getHash}`;
+        const mailBody = signupEmailTemplate(endPoint, newUser.userName);
 
         const mailSent = await sendMail(
-            'hello@simpliclarify.com',
+            'simpliclarify.tech@gmail.com',
             newUser.userEmail,
             'SimpliClariFy Account Verification',
             mailBody
-        )
-        const createdUser = await newUser.save()
-        return createdUser
+        );
+        const createdUser = await newUser.save();
+        return createdUser;
     } catch (error) {
-        throw new CustomError(error)
+        throw new CustomError(error);
     }
 }
 
 async function login(body) {
-    const { userEmail, userPassword } = body
-    const user = await dbCallHandler(User.findOne({ userEmail }), 'User not found')
+    const { userEmail, userPassword } = body;
+    const user = await dbCallHandler(
+        User.findOne({ userEmail }),
+        'User not found'
+    );
     if (user) {
         if (user.isOAuth) {
             throw new CustomError({
                 statusCode: 403,
-                message: 'Trying signing in through google auth'
-            })
+                message: 'Trying signing in through google auth',
+            });
         }
-        const match = await Methods.comparePasswords(userPassword, user.userPassword)
+        const match = await Methods.comparePasswords(
+            userPassword,
+            user.userPassword
+        );
         if (!match) {
             throw new CustomError({
                 statusCode: 403,
                 message: 'Invalid Password',
-                code: 'SIMPLICLARIFY_AUTH_04'
-            })
+                code: 'SIMPLICLARIFY_AUTH_04',
+            });
         } else {
-            return user
+            return user;
         }
     } else {
         throw new CustomError({
             statusCode: 403,
             message: 'Invalid Email Address',
-            code: 'SIMPLICLARIFY_AUTH_04'
-        })
+            code: 'SIMPLICLARIFY_AUTH_04',
+        });
     }
 }
 
 async function verifyUser(body) {
     try {
-        const user = await User.findOne(body)
+        const user = await User.findOne(body);
         if (!user) {
-            throw new CustomError('Looks like link has expired, please try again')
+            throw new CustomError(
+                'Looks like link has expired, please try again'
+            );
         }
         if (user.userVerify === true) {
-            throw new CustomError('You are already verified. Have a good time!')
+            throw new CustomError(
+                'You are already verified. Have a good time!'
+            );
         }
-        user.userVerify = true
+        user.userVerify = true;
         if (user.referralCodeUsed) {
             async function addAmountToWallet(user) {
                 const referrer = await User.findOne({
-                    userReferralCode: user.referralCodeUsed
-                })
+                    userReferralCode: user.referralCodeUsed,
+                });
                 if (referrer) {
-                    referrer.referred.push({ userId: user._id })
-                    referrer.wallet += Constants.referrerAmount
-                    await addTransaction(referrer, Constants.referrerAmount, 'Referral')
-                    await referrer.save()
-                    const subject = 'You just got RICHER!!'
-                    const body = verifyUserEmailTemplate(Constants.referrerAmount)
+                    referrer.referred.push({ userId: user._id });
+                    referrer.wallet += Constants.referrerAmount;
+                    await addTransaction(
+                        referrer,
+                        Constants.referrerAmount,
+                        'Referral'
+                    );
+                    await referrer.save();
+                    const subject = 'You just got RICHER!!';
+                    const body = verifyUserEmailTemplate(
+                        Constants.referrerAmount
+                    );
 
                     const mailSent = await sendMail(
                         'hello@simpliclarify.com',
                         referrer.userEmail,
                         subject,
                         body
-                    )
+                    );
 
-                    user.wallet += Constants.referralAmount
-                    await addTransaction(user, Constants.referralAmount, 'Referral')
-                    return
+                    user.wallet += Constants.referralAmount;
+                    await addTransaction(
+                        user,
+                        Constants.referralAmount,
+                        'Referral'
+                    );
+                    return;
                 }
 
                 const adminReferrer = await Referral.findOne({
-                    code: user.referralCodeUsed
-                })
+                    code: user.referralCodeUsed,
+                });
 
                 if (adminReferrer) {
-                    await addTransaction(user, adminReferrer.value, 'Referral')
-                    user.wallet += adminReferrer.value
+                    await addTransaction(user, adminReferrer.value, 'Referral');
+                    user.wallet += adminReferrer.value;
                 }
             }
-            await addAmountToWallet(user)
+            await addAmountToWallet(user);
         } else {
-            user.wallet += Constants.joiningAmountWithoutReferral
-            await addTransaction(user, Constants.joiningAmountWithoutReferral, 'Signup Reward')
+            user.wallet += Constants.joiningAmountWithoutReferral;
+            await addTransaction(
+                user,
+                Constants.joiningAmountWithoutReferral,
+                'Signup Reward'
+            );
         }
-        return await user.save()
+        return await user.save();
     } catch (error) {
-        throw new CustomError(error)
+        throw new CustomError(error);
     }
 }
 
 async function forgotPassword(body) {
     try {
-        const user = await User.findOne(body)
+        const user = await User.findOne(body);
         if (!user) {
             throw new CustomError({
                 statusCode: 404,
-                message: 'User does not exist'
-            })
+                message: 'User does not exist',
+            });
         }
 
         if (user.isOAuth) {
             throw new CustomError({
                 statusCode: 403,
-                message: 'User cannot reset password. Account logged in through google'
-            })
+                message:
+                    'User cannot reset password. Account logged in through google',
+            });
         }
-        const subject = 'SimpliClariFy Forgot Password'
-        const generatedPassword = _generatePassword(8)
-        const mailBody = forgotPasswordEmailTemplate(user.userName, generatedPassword)
+        const subject = 'SimpliClariFy Forgot Password';
+        const generatedPassword = _generatePassword(8);
+        const mailBody = forgotPasswordEmailTemplate(
+            user.userName,
+            generatedPassword
+        );
         const mailSent = await sendMail(
             'hello@simpliclarify.com',
             user.userEmail,
             subject,
             mailBody
-        )
+        );
         if (mailSent && mailSent.MessageId) {
-            user.userPassword = generatedPassword
-            await user.save()
-            return
+            user.userPassword = generatedPassword;
+            await user.save();
+            return;
         } else {
-            throw new CustomError('Mail Notification Failed')
+            throw new CustomError('Mail Notification Failed');
         }
     } catch (error) {
-        throw new CustomError(error)
+        throw new CustomError(error);
     }
 }
 
@@ -298,16 +335,16 @@ function _generateHash(uniqueHash) {
     const hash = crypto
         .createHash('sha256')
         .update(uniqueHash, 'utf-8')
-        .digest('hex')
-    return hash
+        .digest('hex');
+    return hash;
 }
 
 function _generatePassword(length) {
     const _generatedPassword = generator.generate({
         length,
-        numbers: true
-    })
-    return _generatedPassword
+        numbers: true,
+    });
+    return _generatedPassword;
 }
 
 async function _generateReferralCode() {
@@ -348,38 +385,41 @@ async function _generateReferralCode() {
             'W',
             'X',
             'Y',
-            'Z'
-        ]
-        let str = ''
+            'Z',
+        ];
+        let str = '';
         const array = ['', '', '', '', '', ''].map((i, index) => {
-            str += arr[Math.floor(Math.random() * (35 - index + 1))]
-        })
-        return str
+            str += arr[Math.floor(Math.random() * (35 - index + 1))];
+        });
+        return str;
     }
 
     // Ensuring the referral code is unique
-    let referralCode = null, count = 0
-    const RETRY_COUNT = 10
+    let referralCode = null,
+        count = 0;
+    const RETRY_COUNT = 10;
     while (count++ < RETRY_COUNT) {
-        referralCode = getCode()
+        referralCode = getCode();
         let foundSameReferral = await User.findOne({
-            referralCode: referralCode
-        })
+            referralCode: referralCode,
+        });
         if (!foundSameReferral) {
             foundSameReferral = await Referral.findOne({
-                code: referralCode
-            })
+                code: referralCode,
+            });
         }
-        if (!foundSameReferral) { break }
+        if (!foundSameReferral) {
+            break;
+        }
     }
 
     if (count === RETRY_COUNT) {
         throw new CustomError({
             statusCode: 403,
             message: 'Unable to generate unique referral code',
-        })
+        });
     } else {
-        return referralCode
+        return referralCode;
     }
 }
 
@@ -388,8 +428,8 @@ async function addTransaction(user, amount, purpose) {
         userId: user._id,
         amount: amount,
         type: 'Deposit',
-        purpose
-    }
-    const transaction = new Transaction(transactionBody)
-    await transaction.save()
+        purpose,
+    };
+    const transaction = new Transaction(transactionBody);
+    await transaction.save();
 }
--- email-templates/sendEmailTemplate.js
+++ email-templates/sendEmailTemplate.js
@@ -1,35 +1,47 @@
-import aws from 'aws-sdk'
-import { CustomError } from '../api/helpers/expressUtils'
+import aws from 'aws-sdk';
+import { CustomError } from '../api/helpers/expressUtils';
 
-aws.config.update({ region: 'ap-south-1' })
+// aws.config.update({ region: 'ap-south-1' });
 
-export async function sendMail (Source, recipientEmail, subject, mailBody, BccAddresses) {
-  try {
-    const params = {
-      Source,
-      Destination: {
-        ToAddresses: [
-          recipientEmail
-        ],
-        BccAddresses: BccAddresses && BccAddresses.length > 0 ? BccAddresses : []
-      },
-      ReplyToAddresses: [],
-      Message: {
-        Body: {
-          Html: {
-            Charset: 'UTF-8',
-            Data: mailBody
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: subject
-        }
-      }
+const SES_CONFIG = {
+    accessKeyId: 'AKIAUDE2IDGW6QOSWU74',
+    secretAccessKey: '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',
+    region: 'ap-south-1',
+};
+
+export async function sendMail(
+    Source,
+    recipientEmail,
+    subject,
+    mailBody,
+    BccAddresses
+) {
+    try {
+        const params = {
+            Source,
+            Destination: {
+                ToAddresses: [recipientEmail],
+                BccAddresses:
+                    BccAddresses && BccAddresses.length > 0 ? BccAddresses : [],
+            },
+            ReplyToAddresses: [],
+            Message: {
+                Body: {
+                    Html: {
+                        Charset: 'UTF-8',
+                        Data: mailBody,
+                    },
+                },
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: subject,
+                },
+            },
+        };
+        const mailSent = new aws.SES(SES_CONFIG).sendEmail(params).promise();
+        console.log(mailSent);
+        return mailSent;
+    } catch (error) {
+        throw new CustomError(error);
     }
-    const mailSent = new aws.SES().sendEmail(params).promise()
-    return mailSent
-  } catch (error) {
-    throw new CustomError(error)
-  }
 }
